c8a711efa0afbd7234d81fdcd4da906f98b5ad39
Landyn Pugh <lpugh@ualberta.ca>
2023-03-02 10:37:51 -0700

    Moved audio description data to audio class

--- Server/src/Models/audio.py
+++ Server/src/Models/audio.py
@@ -1,8 +1,8 @@
-from audiodescription import AudioDescription
-
-class Audio:
-    def __init__(self, audioDesc : AudioDescription):
-        self.audioDesc = audioDesc
+class Audio :
+    def __init__(self, samplingRate : int):
+        self.dBData = []
+        self.freqData = []
+        self.samplingRate = samplingRate
 
 
  
--- Server/src/Models/audiodescription.py
+++
@@ -1,6 +0,0 @@
-class AudioDescription:
-    def __init__(self, samplingRate : int):
-        self.dBData = []
-        self.freqData = []
-        self.samplingRate = samplingRate
-    
